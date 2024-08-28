414311b0f5e714cc98df6e1295c7c3072527340c
nirbelelti <nirbelelti@gmail.com>
2024-08-28 11:00:53 +0200

    Create user facade structure

---
+++ facades/user_facade.py
@@ -0,0 +1,19 @@
+
+class UserFacade:
+    def __init__(self):
+        pass
+
+    def get_index(self):
+        return "Hello, World!"
+
+    def create_user(self):
+        return "not implemented yet"
+
+    def get_user_by_id(self, id):
+        return "not implemented yet"
+
+    def update_user(self):
+        return "not implemented yet"
+
+    def delete_user(self):
+        return "not implemented yet"
