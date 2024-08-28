02c32c47fd764411df7b54ddadcb86606506ff50
nirbelelti <nirbelelti@gmail.com>
2024-08-28 10:59:51 +0200

    Add flask and user routes for main.py

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
+
 
 app = Flask(__name__)
 
+@app.route('/users', methods=['GET'])
+def index():
+    return
+@app.route('/user', methods=['GET'])
+def get_user():
+    return
 
+@app.route('/user', methods=['POST'])
+def create():
+    return
+
+@app.route('/user', methods=['PUT'])
+def update():
+    return
+
+@app.route('/user', methods=['DELETE'])
+def delete():
+    return
 
 if __name__ == '__main__':
+
     app.run(debug=True)
 
