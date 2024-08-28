9ffa278133b7e30253a1cb6c8041c42d59cf1c6b
nirbelelti <nirbelelti@gmail.com>
2024-08-28 11:01:49 +0200

    Create user service structure

---
+++ services/user_service.py
@@ -0,0 +1,17 @@
+
+class UserService:
+
+    def index(self):
+        return "Hello, World!"
+
+    def create():
+        return "not implemented yet"
+
+    def get_user(id):
+        return "not implemented yet"
+
+    def update(self):
+        return "not implemented yet"
+
+    def delete(self):
+        return "not implemented yet"
