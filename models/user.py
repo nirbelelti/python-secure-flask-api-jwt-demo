f09c348e6f27fb6068fb8ebd925b94606d8f033b
nirbelelti <nirbelelti@gmail.com>
2024-09-01 18:44:39 +0200

    User model: - Change db configuration to support large binary password column at the DB - Bug fix add the saly to the hash password

--- models/user.py
+++ models/user.py
@@ -4,12 +4,13 @@ import os
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.LargeBinary(64), nullable=False)
 
     def hash_and_salt_password(self, password):
         salt = os.urandom(32)
         hashed_password = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
-        self.password = hashed_password
+        self.password = salt + hashed_password
