22b2c9142edc03d04e3df006990a15fcb4ce6be0
nirbelelti <nirbelelti@gmail.com>
2024-09-06 15:16:52 +0200

    add get user by_id function add update user function

--- facades/user_facade.py
+++ facades/user_facade.py
@@ -9,11 +9,36 @@ class UserFacade:
     def create_user(self):
         return UserService.create(self)
 
-    def get_user_by_id(self, id):
-        return "not implemented yet"
+    @staticmethod
+    def get_user_by_id():
+        user_id = g.decoded_token.get('user_id')
+        user = UserService.get_user(user_id)
+        if user:
+            user_data = {
+                'id': user.id,
+                'username': user.username,
+            }
+            new_token = AuthService().renew_token(
+                {'user_id': user.id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=15),
+                 'jti': str(uuid.uuid4())})
+            response = {
+                'user': user_data,
+                'token': new_token
+            }
+            return response
+        else:
+            return jsonify({'message': 'User not found'}), 404
 
     def update_user(self):
-        return "not implemented yet"
+        UserService.update(self)
+        new_token = AuthService().renew_token(
+            {'user_id': g.current_user.id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=15),
+             'jti': str(uuid.uuid4())})
+        response = {
+            'message': "User updated successfully",
+            'token': new_token
+        }
+        return jsonify(response), 200
 
     def delete_user(self):
         return "not implemented yet"
