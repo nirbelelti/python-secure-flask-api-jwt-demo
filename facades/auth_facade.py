17ba24513c0f2aa0a164a7d10715557d884ef163
nirbelelti <nirbelelti@gmail.com>
2024-09-06 10:39:02 +0200

    Optimise auth Facade Add logout function

--- facades/auth_facade.py
+++ facades/auth_facade.py
@@ -5,29 +5,44 @@ from flask import request
 
 from services.auth_service import AuthService
 
+auth_service = AuthService()
+
 
 class AuthFacade:
-    def __init__(self, auth_service: AuthService):
+
+    def __init__(self):
         self.auth_service = auth_service
 
     def login(self):
-        return AuthService.authenticate_user(self)
+        return auth_service.authenticate_user(request)
 
     def validate_token(self):
         auth_header = request.headers.get('Authorization')
         if auth_header and auth_header.startswith('Bearer '):
             token = auth_header.split(' ')[1]
-            decoded_payload = AuthService.validate_jwt(token)
+            decoded_payload = auth_service.validate_jwt(token)
             if decoded_payload:
                 new_payload = {
                     'user_id': decoded_payload['user_id'],
                     'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=15),
                     'jti': str(uuid.uuid4())
                 }
-                new_token = AuthService.renew_token(new_payload)
+                new_token = auth_service.renew_token(new_payload)
                 return new_token
             else:
                 return {'error': 'Token is invalid or has expired'}, 401
         else:
             return {'error': 'Authorization header missing or invalid'}, 401
 
+    def logout(self):
+        auth_header = request.headers.get('Authorization')
+        if auth_header and auth_header.startswith('Bearer '):
+            token = auth_header.split(' ')[1]
+            decoded_payload = self.auth_service.validate_jwt(token)
+            if decoded_payload:
+                self.auth_service.revoke_token(decoded_payload['jti'])
+                return {'message': 'Token has been revoked'}, 200
+            else:
+                return {'error': 'Token is invalid or has expired'}, 401
+        else:
+            return {'error': 'Authorization header missing or invalid'}, 401
